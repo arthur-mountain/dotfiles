6eb4d687f1f3e65a2303dd660c34d5f69dc30ab3
arthur <arthur.oceanus@gmail.com>
2024-03-18 09:10:18 +0800

    Add telescope keymaps

--- nvim/lua/plugins/telescope.lua
+++ nvim/lua/plugins/telescope.lua
@@ -1,6 +1,6 @@
 return {
   {
-    "telescope.nvim",
+    "nvim-telescope/telescope.nvim",
     dependencies = {
       {
         "nvim-telescope/telescope-fzf-native.nvim",
@@ -13,5 +13,17 @@ return {
         end,
       },
     },
+    opts = function(_, opts)
+      local actions = require("telescope.actions")
+
+      opts.defaults = vim.tbl_deep_extend("force", opts.defaults or {}, {
+        mappings = {
+          i = {
+            ["<C-k>"] = actions.move_selection_previous,
+            ["<C-j>"] = actions.move_selection_next,
+          },
+        },
+      })
+    end,
   },
 }
