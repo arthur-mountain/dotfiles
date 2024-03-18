126dbc560854a7b3f2bde7eaf2121adb4bdf8c25
arthur <arthur.oceanus@gmail.com>
2024-03-18 09:10:49 +0800

    Extract notify

--- nvim/lua/plugins/ui.lua
+++ nvim/lua/plugins/ui.lua
@@ -1,24 +1,4 @@
 return {
-  --------- messages, cmdline and the popupmenu ---------
-  {
-    "folke/noice.nvim",
-    opts = function(_, opts)
-      table.insert(opts.routes, {
-        filter = {
-          event = "notify",
-          find = "No information available",
-        },
-        opts = { skip = true },
-      })
-
-      opts.presets.lsp_doc_border = true
-    end,
-  },
-  {
-    "rcarriga/nvim-notify",
-    opts = { timeout = 5000 },
-  },
-
   --------- animations ---------
   {
     "echasnovski/mini.animate",
