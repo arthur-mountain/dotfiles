8d9b4ee58fd68d8fbd5b436059288298cea2a3b8
arthur <arthur.oceanus@gmail.com>
2024-03-18 09:24:03 +0800

    Add vertical help

--- nvim/lua/config/autocmds.lua
+++ nvim/lua/config/autocmds.lua
@@ -34,3 +34,14 @@ vim.api.nvim_create_autocmd("BufReadPost", {
     end
   end,
 })
+
+-- Vertical help window
+vim.api.nvim_create_autocmd("FileType", {
+  group = vim.api.nvim_create_augroup("vertical_help", { clear = true }),
+  pattern = "help",
+  callback = function()
+    vim.bo.bufhidden = "unload"
+    vim.cmd.wincmd("L")
+    vim.cmd.wincmd("=")
+  end,
+})
