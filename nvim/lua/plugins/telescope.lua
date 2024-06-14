ea2793af0eaa5013a606654067acedda621ed2c0
arthur <arthur.oceanus@gmail.com>
2024-06-14 14:56:22 +0800

    remove telescope-fzf-native tempriory

--- nvim/lua/plugins/telescope.lua
+++ nvim/lua/plugins/telescope.lua
@@ -2,19 +2,19 @@ return {
   {
     "nvim-telescope/telescope.nvim",
     dependencies = {
-      {
-        "nvim-telescope/telescope-fzf-native.nvim",
-        build = "make",
-        enabled = vim.fn.executable("make") == 1,
-        config = function()
-          LazyVim.on_load("telescope.nvim", function()
-            local ok, err = pcall(require("telescope").load_extension, "fzf")
-            if not ok then
-              LazyVim.error("Failed to load `telescope-fzf-native.nvim`:\n" .. err)
-            end
-          end)
-        end,
-      },
+      -- {
+      --   "nvim-telescope/telescope-fzf-native.nvim",
+      --   build = "make",
+      --   enabled = vim.fn.executable("make") == 1,
+      --   config = function()
+      --     LazyVim.on_load("telescope.nvim", function()
+      --       local ok, err = pcall(require("telescope").load_extension, "fzf")
+      --       if not ok then
+      --         LazyVim.error("Failed to load `telescope-fzf-native.nvim`:\n" .. err)
+      --       end
+      --     end)
+      --   end,
+      -- },
     },
     opts = function(_, opts)
       local actions = require("telescope.actions")
