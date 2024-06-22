5fb8c4797c6e18698af85c27e02b938841d7441d
arthur <arthur.oceanus@gmail.com>
2024-06-22 09:58:58 +0800

    update extras management

--- nvim/lua/config/lazy.lua
+++ nvim/lua/config/lazy.lua
@@ -11,36 +11,36 @@ require("lazy").setup({
     -- add LazyVim and import its plugins --
     { "LazyVim/LazyVim", import = "lazyvim.plugins" },
     -- import any extras modules here --
-    -- All
-    { import = "lazyvim.plugins.extras.coding.copilot" },
-    { import = "lazyvim.plugins.extras.coding.mini-surround" },
-    -- { import = "lazyvim.plugins.extras.coding.copilot-chat" },
-    { import = "lazyvim.plugins.extras.linting.eslint" },
-    { import = "lazyvim.plugins.extras.formatting.prettier" },
-    { import = "lazyvim.plugins.extras.util.mini-hipatterns" },
-    { import = "lazyvim.plugins.extras.editor.aerial" },
-    { import = "lazyvim.plugins.extras.test.core" },
-    { import = "lazyvim.plugins.extras.dap.core" },
-    -- UI
-    { import = "lazyvim.plugins.extras.ui.mini-indentscope" },
-    { import = "lazyvim.plugins.extras.ui.mini-animate" },
-    { import = "lazyvim.plugins.extras.ui.treesitter-context" },
-    -- Frontend
-    { import = "lazyvim.plugins.extras.lang.typescript" },
-    { import = "lazyvim.plugins.extras.lang.tailwind" },
-    -- Backend
-    { import = "lazyvim.plugins.extras.lang.go" },
-    { import = "lazyvim.plugins.extras.lang.sql" },
-    -- { import = "lazyvim.plugins.extras.lang.java" },
-    -- { import = "lazyvim.plugins.extras.lang.kotlin" },
-    -- { import = "lazyvim.plugins.extras.lang.haskell" },
-    -- { import = "lazyvim.plugins.extras.lang.rust" },
-    -- { import = "lazyvim.plugins.extras.lang.cmake" },
-    -- Devops
-    { import = "lazyvim.plugins.extras.lang.docker" },
-    { import = "lazyvim.plugins.extras.lang.json" },
-    { import = "lazyvim.plugins.extras.lang.yaml" },
-    { import = "lazyvim.plugins.extras.lang.markdown" },
+    -- -- All
+    -- { import = "lazyvim.plugins.extras.coding.copilot" },
+    -- { import = "lazyvim.plugins.extras.coding.mini-surround" },
+    -- -- { import = "lazyvim.plugins.extras.coding.copilot-chat" },
+    -- { import = "lazyvim.plugins.extras.linting.eslint" },
+    -- { import = "lazyvim.plugins.extras.formatting.prettier" },
+    -- { import = "lazyvim.plugins.extras.util.mini-hipatterns" },
+    -- { import = "lazyvim.plugins.extras.editor.aerial" },
+    -- { import = "lazyvim.plugins.extras.test.core" },
+    -- { import = "lazyvim.plugins.extras.dap.core" },
+    -- -- UI
+    -- { import = "lazyvim.plugins.extras.ui.mini-indentscope" },
+    -- { import = "lazyvim.plugins.extras.ui.mini-animate" },
+    -- { import = "lazyvim.plugins.extras.ui.treesitter-context" },
+    -- -- Frontend
+    -- { import = "lazyvim.plugins.extras.lang.typescript" },
+    -- { import = "lazyvim.plugins.extras.lang.tailwind" },
+    -- -- Backend
+    -- { import = "lazyvim.plugins.extras.lang.go" },
+    -- { import = "lazyvim.plugins.extras.lang.sql" },
+    -- -- { import = "lazyvim.plugins.extras.lang.java" },
+    -- -- { import = "lazyvim.plugins.extras.lang.kotlin" },
+    -- -- { import = "lazyvim.plugins.extras.lang.haskell" },
+    -- -- { import = "lazyvim.plugins.extras.lang.rust" },
+    -- -- { import = "lazyvim.plugins.extras.lang.cmake" },
+    -- -- Devops
+    -- { import = "lazyvim.plugins.extras.lang.docker" },
+    -- { import = "lazyvim.plugins.extras.lang.json" },
+    -- { import = "lazyvim.plugins.extras.lang.yaml" },
+    -- { import = "lazyvim.plugins.extras.lang.markdown" },
     -- import/override with your plugins --
     { import = "plugins" },
   },
