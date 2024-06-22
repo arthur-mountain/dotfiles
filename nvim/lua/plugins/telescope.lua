ba44e27e38ffdc04d364cac7b0c4d49dd44bfd93
arthur <arthur.oceanus@gmail.com>
2024-06-22 13:01:06 +0800

    add telescope extra but migrate to fzf-lua in the future

--- nvim/lua/plugins/telescope.lua
+++ nvim/lua/plugins/telescope.lua
@@ -1,21 +1,6 @@
 return {
   {
     "nvim-telescope/telescope.nvim",
-    dependencies = {
-      -- {
-      --   "nvim-telescope/telescope-fzf-native.nvim",
-      --   build = "make",
-      --   enabled = vim.fn.executable("make") == 1,
-      --   config = function()
-      --     LazyVim.on_load("telescope.nvim", function()
-      --       local ok, err = pcall(require("telescope").load_extension, "fzf")
-      --       if not ok then
-      --         LazyVim.error("Failed to load `telescope-fzf-native.nvim`:\n" .. err)
-      --       end
-      --     end)
-      --   end,
-      -- },
-    },
     opts = function(_, opts)
       local actions = require("telescope.actions")
 
