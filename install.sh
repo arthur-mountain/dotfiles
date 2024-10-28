a36567b36a8b7452b589c4eed1d737ddbc671e76
arthur <arthur.oceanus@gmail.com>
2024-10-28 10:58:09 +0800

    updated indent

--- install.sh
+++ install.sh
@@ -4,45 +4,45 @@
 . ./theme.sh
 
 run() {
-	local title="$1"
-	local setup="$2"
+  local title="$1"
+  local setup="$2"
 
-	echo -e "$(gray "========= $(purple "Setting up $title START") =========")"
+  echo -e "$(gray "========= $(purple "Setting up $title START") =========")"
 
-	$setup
+  $setup
 
-	echo -e "$(gray "========= $(purple "Setting up $title END")   =========")"
+  echo -e "$(gray "========= $(purple "Setting up $title END")   =========")"
 }
 
 error() {
-	echo -e "$(red "Error:") $1"
-	exit 1
+  echo -e "$(red "Error:") $1"
+  exit 1
 }
 
 warning() {
-	echo -e "$(yellow "Warning:") $1"
+  echo -e "$(yellow "Warning:") $1"
 }
 
 info() {
-	echo -e "$(blue "Info:") $1"
+  echo -e "$(blue "Info:") $1"
 }
 
 success() {
-	echo -e "$(green "$1")"
+  echo -e "$(green "$1")"
 }
 
 ####################################
 ############ Homebrew ##############
 ####################################
 setup_homebrew() {
-	if test ! "$(command -v brew)"; then
-		info "Homebrew not installed. Installing."
-		# Run as a login shell (non-interactive) so that the script doesn't pause for user input
-		curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install.sh | bash --login
-	fi
+  if test ! "$(command -v brew)"; then
+    info "Homebrew not installed. Installing."
+    # Run as a login shell (non-interactive) so that the script doesn't pause for user input
+    curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install.sh | bash --login
+  fi
 
-	# install brew dependencies from Brewfile
-	brew bundle
+  # install brew dependencies from Brewfile
+  brew bundle
 }
 
 # echo -e "Enter the path to the dotfiles directory: "
@@ -50,20 +50,20 @@ setup_homebrew() {
 # echo -e
 
 if [[ "$DOTFILES" == "" ]]; then
-	DOTFILES="$(pwd)"
+  DOTFILES="$(pwd)"
 fi
 
 case "$1" in
 homebrew)
-	# run "Setting up Homebrew" setup_homebrew
-	;;
+  # run "Setting up Homebrew" setup_homebrew
+  ;;
 zsh)
-	run "ZSH" ". ./zsh/setup.sh"
-	;;
+  run "ZSH" ". ./zsh/setup.sh"
+  ;;
 *)
-	echo -e $"\n$(red "Usage"): $(basename "$0") {homebrew|zsh}\n"
-	exit 1
-	;;
+  echo -e $"\n$(red "Usage"): $(basename "$0") {homebrew|zsh}\n"
+  exit 1
+  ;;
 esac
 
 success "\nDone."
