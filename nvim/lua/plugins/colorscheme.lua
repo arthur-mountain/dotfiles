7594c5dc58db6d3388c48ffecaafb73529871dd6
arthur <arthur.oceanus@gmail.com>
2024-12-29 15:25:21 +0800

    update color scheme

--- nvim/lua/plugins/colorscheme.lua
+++ nvim/lua/plugins/colorscheme.lua
@@ -3,16 +3,19 @@ return {
     "folke/tokyonight.nvim",
     opts = {
       style = "storm", --  `storm`, `moon`, a darker variant `night` and `day`
+      light_style = "day",
       transparent = true,
+      terminal_colors = true,
       lualine_bold = true,
       styles = { sidebars = "transparent", floats = "transparent" },
-      on_colors = function(colors)
-        colors.comment = "#C0C0C0"
-      end,
+      -- on_colors = function(colors) end,
       on_highlights = function(hl, colors)
         -- Relative number
-        hl.LineNr = { fg = colors.blue }
+        hl.LineNr = { fg = colors.purple, bold = true }
         hl.CursorLineNr = { fg = colors.orange }
+        -- Comment
+        colors.comment = "#A0A0A0"
+        hl.Comment = { bg = "#4B5268", fg = colors.comment, italic = true }
       end,
     },
   },
