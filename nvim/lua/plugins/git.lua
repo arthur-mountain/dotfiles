c47c1f1e261113971cf4c55c4a2ed5b9bdcac167
arthur <arthur.oceanus@gmail.com>
2025-04-12 12:55:19 +0800

    remove unnecessary setup

--- nvim/lua/plugins/git.lua
+++ nvim/lua/plugins/git.lua
@@ -1,18 +1,4 @@
 return {
-  --------- lazygit ---------
-  {
-    "kdheepak/lazygit.nvim",
-    dependencies = {
-      "nvim-lua/plenary.nvim",
-      "nvim-telescope/telescope.nvim",
-    },
-    config = function()
-      require("lazyvim.util").on_load("telescope.nvim", function()
-        require("telescope").load_extension("lazygit")
-      end)
-    end,
-  },
-
   --------- git signs ---------
   {
     "lewis6991/gitsigns.nvim",
--- nvim/lua/plugins/lsp.lua
+++ nvim/lua/plugins/lsp.lua
@@ -3,8 +3,6 @@ return {
     "williamboman/mason.nvim",
     opts = function(_, opts)
       vim.list_extend(opts.ensure_installed, {
-        "luacheck",
-        "shellcheck",
         "css-lsp",
         "mdx-analyzer",
       })
