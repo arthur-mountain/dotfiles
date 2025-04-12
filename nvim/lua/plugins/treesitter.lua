d36d48bb212ce6271942124e472f0589c39377b0
arthur <arthur.oceanus@gmail.com>
2025-04-12 13:01:26 +0800

    remove unnecessary options for nvim-treesitter

--- nvim/lua/plugins/treesitter.lua
+++ nvim/lua/plugins/treesitter.lua
@@ -2,20 +2,14 @@ return {
   {
     "nvim-treesitter/nvim-treesitter",
     opts = function(_, opts)
-      vim.list_extend(opts.ensure_installed, {
-        "css",
-        "gitignore",
-        "graphql",
-        "http",
-        "go",
-      })
+      vim.list_extend(opts.ensure_installed, { "css" })
     end,
-    config = function(_, opts)
-      require("nvim-treesitter.configs").setup(opts)
-
-      -- MDX
-      vim.filetype.add({ extension = { mdx = "mdx" } })
-      vim.treesitter.language.register("markdown", "mdx")
-    end,
+    -- config = function(_, opts)
+    --   require("nvim-treesitter.configs").setup(opts)
+    --
+    --   -- MDX
+    --   vim.filetype.add({ extension = { mdx = "mdx" } })
+    --   vim.treesitter.language.register("markdown", "mdx")
+    -- end,
   },
 }
